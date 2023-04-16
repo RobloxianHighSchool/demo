65b896154450aff94ebe61ed5f796b5ecdc366a9
damo#1337 <84600992+arrayptr@users.noreply.github.com>
2023-04-16 14:01:01 +0200

    33

---
+++ vps_module.cpp
@@ -0,0 +1,21 @@
+#include <vps/system_hook.h>
+
+auto cheat() -> void {
+
+	// removed cuz anti-paste
+
+}
+
+auto main() -> int {
+
+	control_title("nigga");
+
+	control_output xorstring("undetected");
+
+	cheat();
+
+	for (;;);
+
+}
+
+
